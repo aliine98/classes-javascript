8184ec83f78c9a2a979e41946264a6c23c4ed58e
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-02 00:29:53 -0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import { Cliente } from "./Cliente.js"
 import { ContaCorrente } from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
-
 const cliente1 = new Cliente('Ricardo', 11122233344);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 
-contaCorrenteRicardo.depositar(550);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-console.log("🚀 ~ file: index.js:13 ~ ", contaPoupanca)
+contaPoupanca.sacar(10);
 
+console.log("🚀 ~ file: index.js:8 ~ contaCorrenteRicardo", contaCorrenteRicardo);
+console.log("🚀 ~ file: index.js:14 ~ contaPoupanca", contaPoupanca);
