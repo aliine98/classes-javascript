30ca6460b985eac2d36bacbcf93e91a91a0c8521
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-02 00:04:48 -0300

    melhorando classe usando herança e super constructor

--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -1,27 +1,5 @@
 export class ContaPoupanca {
    constructor(saldoInicial, cliente, agencia) {
-      this._saldo = saldoInicial;
-      this._cliente = cliente;
-      this._agencia = agencia;
-   }
-
-   depositar(valor) {
-      //early return, verifica uma condição para interromper a execução do método/função
-      if (valor <= 0) {
-         return;
-      }
-      this._saldo += valor;
-   }
-
-   sacar(valor) {
-      if (this._saldo >= valor) {
-         this._saldo -= valor;
-         return valor;
-      }
-   }
-
-   transferir(valor, conta) {
-      const valorSacado = this.sacar(valor);
-      conta.depositar(valorSacado);
+      super(saldoInicial, cliente, agencia);
    }
 }
