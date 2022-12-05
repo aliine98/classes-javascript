2478dd134b9cba611ceca10235efb5ff9d642fc7
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-05 00:03:39 -0300

    fix: atribuindo valores aos atributos

--- Funcionario/Funcionario.js
+++ Funcionario/Funcionario.js
@@ -1,7 +1,7 @@
 export class Funcionario {
    constructor(nome, salario, cpf) {
-      this._nome;
-      this._salario;
-      this._cpf
+      this._nome = nome;
+      this._salario = salario;
+      this._cpf = cpf;
    }
 }
