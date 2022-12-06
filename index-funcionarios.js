ac48aaf8017c123896b8b443c434136ee0223951
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-06 01:45:27 -0300

    :sparkles: feat: nova instância de diretor e gerente, cadastro e autentificação de login simples

--- index-funcionarios.js
+++ index-funcionarios.js
@@ -1,3 +1,11 @@
 import { Cliente } from "./Cliente.js";
 import { Diretor } from "./Funcionario/Diretor.js";
 import { Gerente } from "./Funcionario/Gerente.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+
+const diretor = new Diretor('Rodrigo', 10000, 12345678900);
+diretor.cadastrarSenha('123456789');
+const gerente = new Gerente('Aline', 5000, 12378945601);
+
+const estaLogado = SistemaAutenticacao.login(diretor, '123456789');
+console.log("🚀 ~ file: index-funcionarios.js:9 ~ estaLogado", estaLogado);
