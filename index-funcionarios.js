2270cbe71bae84f8156b13604935db5019872006
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-08 01:30:55 -0300

    :sparkles: feat: criando novo cliente

--- index-funcionarios.js
+++ index-funcionarios.js
@@ -6,6 +6,12 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 const diretor = new Diretor('Rodrigo', 10000, 12345678900);
 diretor.cadastrarSenha('123456789');
 const gerente = new Gerente('Aline', 5000, 12378945601);
+gerente.cadastrarSenha('123');
 
-const estaLogado = SistemaAutenticacao.login(diretor, '123456789');
-console.log("🚀 ~ file: index-funcionarios.js:9 ~ estaLogado", estaLogado);
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123');
+console.log("🚀 ~ file: index-funcionarios.js:12 ~ gerenteEstaLogado", gerenteEstaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456789');
+console.log("🚀 ~ file: index-funcionarios.js:14 ~ diretorEstaLogado", diretorEstaLogado);
+const cliente = new Cliente('Lais', 78945612379, '456');
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, '456');
+console.log("🚀 ~ file: index-funcionarios.js:17 ~ clienteEstaLogado", clienteEstaLogado);
