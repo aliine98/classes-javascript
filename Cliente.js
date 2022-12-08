7b89355c2ebf0fca5d3e4d2c054acd84a66962b7
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-08 01:12:18 -0300

    :ok_hand: style: constructor movido para início da classe

--- Cliente.js
+++ Cliente.js
@@ -1,10 +1,11 @@
 export class Cliente {
-   get cpf() {
-      return this._cpf;
-   }
-
-   constructor(nome, cpf) {
+   constructor(nome, cpf,) {
       this.nome = nome;
       this._cpf = cpf;
+      tis._senha = senha;
+   }
+
+   get cpf() {
+      return this._cpf;
    }
 }
